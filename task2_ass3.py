d4900d4553f843983cc427bebf99e3ada7e73fc2
Ankit6174-cmd <tiwari.ankit225@gmail.com>
2025-07-27 13:31:14 +0530

    Add files via upload

---
+++ task2_ass3.py
@@ -0,0 +1,15 @@
+n=int(input("Enter a number: "))
+import math #using math module
+
+#for calculating square root
+result_sqrt = math.sqrt(n)
+print("square root: ",result_sqrt)
+
+#for calculating natural logarithm
+result_log = math.log(n)
+print("logarithm: ",result_log)
+
+#for calculating sine of a number
+result_sine = math.sin(n)
+print("sine: ",result_sine)
+
